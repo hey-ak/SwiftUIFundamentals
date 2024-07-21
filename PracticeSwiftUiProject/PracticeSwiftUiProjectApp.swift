a3c46f1cba8cc176f3c0d647bfc59f7a94311e73
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-07-21 09:00:03 +0530

    Initial Commit

---
+++ PracticeSwiftUiProject/PracticeSwiftUiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUiProjectApp.swift
+//  PracticeSwiftUiProject
+//
+//  Created by Akshay Jha on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
